f3e6431fe085bfce027d638b51e6170e4500f0df
zeynepakkaya-1 <zeynepxakkaya21@gmail.com>
2023-02-13 22:00:25 +0300

    dictionaries.py added

---
+++ dictionaries.py
@@ -0,0 +1,63 @@
+# key: value
+# Dictionary items are ordered, changeable, and does not allow duplicates.
+
+# dict() constructor
+print(dict(name = "Z", age = 21, country = "Turkey")) # {'name': 'Z', 'age': 21, 'country': 'Turkey'}
+
+fordmustang_dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1970,
+  "year": 2023 # overwrite existing value
+}
+
+print(fordmustang_dict["brand"]) # Ford
+
+# .get()
+print(fordmustang_dict.get("brand")) # Ford
+
+# .keys()
+print(fordmustang_dict.keys()) # dict_keys(['brand', 'model', 'year'])
+
+# .values()
+print(fordmustang_dict.values()) # dict_values(['Ford', 'Mustang', 2023])
+
+# .items()
+print(fordmustang_dict.items()) # dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 2023)])
+
+for x, y in fordmustang_dict.items():
+  print(x, y)
+"""
+brand Ford
+model Mustang
+year 2023
+"""
+
+# .setdefault()
+print(fordmustang_dict.setdefault("year", 1970)) # 2023
+# If the key does not exist, insert the key with the specified value.
+
+# .update()
+fordmustang_dict.update({"price": "$35.000"})
+print(fordmustang_dict) # {'brand': 'Ford', 'model': 'Mustang', 'year': 2023, 'price': '$35.000'}
+
+# .pop()
+print(fordmustang_dict.pop("brand")) # Ford
+print(fordmustang_dict) # {'model': 'Mustang', 'year': 2023, 'price': '$35.000'}
+
+#.popitem()
+print(fordmustang_dict.popitem()) # ('price', '$35.000')
+print(fordmustang_dict) # {'model': 'Mustang', 'year': 2023}
+
+# del keyword
+del fordmustang_dict["year"]
+print(fordmustang_dict) # {'model': 'Mustang'}
+
+fordmustang_dict.clear()
+del fordmustang_dict
+
+# .fromkeys()
+x = ('key1', 'key2', 'key3')
+y = 0
+thisdict = dict.fromkeys(x, y)
+print(thisdict) # {'key1': 0, 'key2': 0, 'key3': 0}
