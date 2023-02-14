b2dbf322d9cd4904d69400caf3116af2574f41d2
zeynepakkaya-1 <zeynepxakkaya21@gmail.com>
2023-02-14 16:12:36 +0300

    Try Except added

---
+++ try_except.py
@@ -0,0 +1,30 @@
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined") #
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+finally:
+  print("The 'try except' is finished") #
+
+# files
+try:
+  f = open("file.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file") #
+
+x = 1
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+
+x = 1
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
