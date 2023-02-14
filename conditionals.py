da07ab5e00fda5e3ee89b25d4aa9140c39ffbf98
zeynepakkaya-1 <zeynepxakkaya21@gmail.com>
2023-02-14 16:13:24 +0300

    Conditionals added

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+if 10 > 5: print("10 is greater than 5")
+print("10 is greater than 5") if 10 > 5 else print("5 is greater than 10")
+print("10") if 10 > 5 else print("=") if 10 == 5 else print("5")
+
+# pass keyword
+if 100 > 50: pass
+
+for x in [0, 1, 2]: pass # avoid getting an error with pass
