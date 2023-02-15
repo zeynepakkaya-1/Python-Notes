693acc07664f31a4feffa4540cc60c17b380b0b8
zeynepakkaya-1 <zeynepxakkaya21@gmail.com>
2023-02-15 21:05:58 +0300

    Importing a module

---
+++ modules/import/modules.py
@@ -0,0 +1,7 @@
+import greeting_function as greet
+# from greeting_function import greeting
+
+greet.greeting("Admin") # Hello, Admin
+
+print(dir(greet))
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'greeting']
